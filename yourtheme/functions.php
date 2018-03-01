9948929c75af394ef1e7d44bb7463917987b0fc8
Benjamin Zekavica <info@benjamin-zekavica.de>
2018-03-01 21:41:43 +0100

    Add Single Page

--- yourtheme/functions.php
+++ yourtheme/functions.php
@@ -1,57 +1,55 @@
-<?php 
+<?php
 
-/* 
+/*
   Theme Name: Your Theme
   Description: Add Functions
   Author: Benjamin Zekavica
-  Version: 1.0 
-*/ 
+  Version: 1.0
+*/
 
-// Add WP Job Manger Function 
+// Add WP Job Manger Function
 
 add_theme_support( 'job-manager-templates' );
 
 
-/* Add Fields to Job Manager */ 
+/* Add Fields to Job Manager */
 
 add_filter( 'job_manager_job_listing_data_fields', 'mks_feld' );
 
 
 function mks_feld( $fields ) {
- 
+
 
   // Add Postleitzahl
   $fields['_plz'] = array(
     'label'       => __( 'Postleitzahl', 'job_manager' ),
     'type'        => 'text',
     'placeholder' => '52066',
-    'description' => 'Geben Sie hier eine Postleitzahl ein', 
+    'description' => 'Geben Sie hier eine Postleitzahl ein',
   );
 
-  // Begin the job  
+  // Begin the job
   $fields['_beginjob'] = array(
     'label'       => __( 'Beginn der Arbeit', 'job_manager' ),
     'type'        => 'text',
     'placeholder' => '',
-    'description' => 'Wann startet der Job?', 
+    'description' => 'Wann startet der Job?',
     'classes'     => array( 'job-manager-datepicker' ),
   );
-	
-  // Begin the job  
+
+  // Begin the job
   $fields['_endjob'] = array(
     'label'       => __( 'Ende der Arbeit', 'job_manager' ),
     'type'        => 'text',
     'placeholder' => '',
-    'description' => 'Wann endet der Job?', 
+    'description' => 'Wann endet der Job?',
     'classes'     => array( 'job-manager-datepicker' ),
   );
 
   return $fields;
 }
 
-
-
-// Enque Files 
+// Enque Files
 
 function bz_enque() {
     wp_enqueue_style( 'style-name', get_stylesheet_uri() );
@@ -59,4 +57,3 @@ function bz_enque() {
 }
 
 add_action( 'wp_enqueue_scripts', 'bz_enque' );
- 
---
+++ yourtheme/single-job_listing.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="job-title">
+  <?php the_title(); ?>
+</div>
+
+
+<div class="job-description">
+  <?php the_content(); ?>
+</div>
+
+<!-- Hier kann man das Markup bearbeiten  -->
+
+<?php get_header(); ?>
