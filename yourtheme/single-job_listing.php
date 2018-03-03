d1b80db810cc7e2f41c25a50bd99354b05fb907d
Benjamin Zekavica <info@benjamin-zekavica.de>
2018-03-03 12:27:32 +0100

    Add Require Once

--- yourtheme/single-job_listing.php
+++ yourtheme/single-job_listing.php
@@ -1,14 +1,9 @@
 <?php get_header(); ?>
+	
+	<!-- Hier soll das Single Template vom Job Manager hinzugefügt werden
+		//Hinweis: Diese Datei muss im Theme stehen, sonst wird das Template vom Plugin übernommen
+   	-->
 
-<div class="job-title">
-  <?php the_title(); ?>
-</div>
-
-
-<div class="job-description">
-  <?php the_content(); ?>
-</div>
-
-<!-- Hier kann man das Markup bearbeiten  -->
-
+	<?php require_once('job_manger/content-single-job_listing.php'); ?>
+	
 <?php get_header(); ?>
